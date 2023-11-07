8cebcbe08df05923935ba9e204d2839d6fc845c6
Andrés Hernández <65791808+andyherfer@users.noreply.github.com>
2023-11-07 06:20:58 +0000

    dev

--- comment.py
+++ comment.py
@@ -12,7 +12,7 @@ class GithubHandler:
 
         self.repo_name = repo_name
         # get repo
-        self.repo = g.get_repo(self.repo_name)
+        self.repo = self.g.get_repo(self.repo_name)
         # get prs
         self.prs = self.repo.get_pulls(state = "open", sort = "created", base = "master")
         # get prs numbers
@@ -29,7 +29,7 @@ class GithubHandler:
 
 if __name__ == "__main__":
     print(os.environ["GH_ACCESSTOKEN"])
-    print(os.environ["pull_request_number"])
+    #print(os.environ["pull_request_number"])
     GH = GithubHandler()
     
         
