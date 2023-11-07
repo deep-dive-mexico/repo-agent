0bd49b8db233c6bd2e9f2454d061c54561eb9661
andyherfer <andyherfer@yahoo.com.mx>
2023-11-07 01:06:48 -0600

    Co-authored-by: Sergio Arnaud Gomez <sercharnaud@gmail.com> Co-authored-by: MAXCASMAR <MAXCASMAR@users.noreply.github.com>

--- comment.py
+++ comment.py
@@ -23,14 +23,18 @@ class GithubHandler:
         self.prs_nums = [pr.number for pr in self.prs]
         # create dict of prs
         self.prs_dict = {k: v for k, v in zip(self.prs_nums, self.prs)}
+
         # get all files
         # self.get_all_files()
-
 
     def authenticate(self):
         # github authentication
         self.auth = Auth.Token(ACCESS_TOKEN)
         self.g = Github(auth=self.auth)
+
+    def get_latest_pr_from_branch(self, branch: str):
+        prs = self.repo.get_pulls(state = "open", sort = "created", base = branch)
+        return prs[0]
 
     def get_all_files(self):
         contents = self.repo.get_contents("")
@@ -41,7 +45,7 @@ class GithubHandler:
                 contents.extend(self.repo.get_contents(file_content.path))
             
         self.all_files = contents
-    
+
     def get_pr_changes(self, pr_num: int):
         pr = self.prs_dict[pr_num]
         files_and_deltas = ""
@@ -54,20 +58,20 @@ class GithubHandler:
         pass
 
     def get_file_contents(self, filepath: str):
-        if filepath in self.all_files:
-            file_contents = self.repo.get_contents(filepath)
-        else:
-            file_contents = ""
 
-        return file_contents
+        file_contents = self.repo.get_contents(filepath)
+
+        return file_contents.decoded_content.decode()
     
     def update_file_contents(self, filepath: str):
         contents = self.repo.get_contents(filepath, ref)
 
 
+
 if __name__ == "__main__":
-    print(os.environ["GH_ACCESSTOKEN"])
-    print(os.environ["PR_NUMBER"])
-    GH = GithubHandler()
+    gh_access = os.environ["GH_ACCESSTOKEN"]
+    branch_or_pr = os.environ["PR_NUMBER"]
+    repo_name = os.environ["REPO_NAME"]
+    GH = GithubHandler(repo_name=repo_name)
     
         
