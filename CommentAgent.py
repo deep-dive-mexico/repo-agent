4c7cc436b527bfaa4db4f901e4e7a77559d74bfb
Andrés Hernández <65791808+andyherfer@users.noreply.github.com>
2023-11-13 22:55:41 -0600

    Update CommentAgent.py

--- CommentAgent.py
+++ CommentAgent.py
@@ -171,7 +171,9 @@ class ResponseParser:
             return python_code_block
         else:
             raise ParsingError(
-                "No python snippet provided, remember to use ```python\n<valid_python_code>\n```"
+        raise ParsingError(
+"No python snippet provided. Please include a python code block using the markdown format ```python ... ``` for clarity and proper parsing."
+        )
             )
 
     def get_body_event_and_comments(self):
