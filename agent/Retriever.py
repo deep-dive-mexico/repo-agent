b2b2e2d01deaeedf9341562cd8c4c348944e7bb7
Inaki-FF <fernandez.fiscal.inaki@gmail.com>
2023-11-24 15:41:13 -0600

    docstrings and refactor

--- agent/Retriever.py
+++ agent/Retriever.py
@@ -1,65 +1,83 @@
 from llama_index import (
     VectorStoreIndex,
     SummaryIndex,
-    SimpleKeywordTableIndex,
     SimpleDirectoryReader,
-    ServiceContext,
+    ServiceContext
 )
 from llama_index.agent import OpenAIAgent
 from llama_index.schema import IndexNode
 from llama_index.tools import QueryEngineTool, ToolMetadata
 from llama_index.llms import OpenAI
-import os
 from llama_index.retrievers import RecursiveRetriever
 from llama_index.query_engine import RetrieverQueryEngine
 from llama_index.response_synthesizers import get_response_synthesizer
 
+
 class Retriever:
+    """
+    A class to handle the retrieval and querying of documents using Llama Index services.
+
+    Attributes:
+        llm (OpenAI): An instance of the OpenAI model.
+        service_context (ServiceContext): The context for the service.
+        docs (dict): A dictionary holding the loaded documents.
+        query_engine (RetrieverQueryEngine): The query engine for retrieval tasks.
+    """
+
     def __init__(self, index_path):
-        
+        """
+        Initialize the Retriever with a specified index path.
+
+        Args:
+            index_path (str): The path to the index directory.
+        """
         self.llm = OpenAI(temperature=0, model="gpt-4-1106-preview")
         self.service_context = ServiceContext.from_defaults(llm=self.llm)
-    
-    def load_documents(self, files):
         self.docs = {}
-        for file in files.keys():
-            docs[file] = SimpleDirectoryReader(
-                input_files=[f"./{file}"],
+        self.query_engine = None
+
+    def load_documents(self, files):
+        """
+        Load documents from the given file paths.
+
+        Args:
+            files (list): A list of file paths to load.
+        """
+        for file in files:
+            self.docs[file] = SimpleDirectoryReader(
+                input_files=[f"./{file}"]
             ).load_data()
 
-
-
-    def create_agents(self, files, docs, service_context):
-            summary_index = SummaryIndex.from_documents(docs[file], service_context=service_context)
-            
-            # define query engines
-            vector_query_engine = vector_index.as_query_engine()
+    def create_agents(self):
+        """
+        Create agents for each loaded document.
+        """
+        self.agents = {}
+        for file, doc in self.docs.items():
+            summary_index = SummaryIndex.from_documents(doc, service_context=self.service_context)
             list_query_engine = summary_index.as_query_engine()
-            
-            # define tools
+
             query_engine_tools = [
                 QueryEngineTool(
-                    query_engine=vector_query_engine,
-                    metadata=ToolMetadata(name="vector_tool", description=f"Useful for summarization questions related to {file}"),
-                ),
-                QueryEngineTool(
                     query_engine=list_query_engine,
-                    metadata=ToolMetadata(name="summary_tool", description=f"Useful for retrieving specific context from {file}"),
-                ),
+                    metadata=ToolMetadata(name="summary_tool", description=f"Retrieve context from {file}")
+                )
             ]
-            
-            # build agent
+
             function_llm = OpenAI(model="gpt-4-1106-preview")
-            agent = OpenAIAgent.from_tools(query_engine_tools, llm=function_llm, verbose=True)
-            
-            agents[file] = agent
-    
-    def create_query_engine(_files, _agents, _service_context):
-        nodes = []
-        for file in files.keys():
-            file_summary = f"This content contains Law Information  about {file}. Use this index if you need to lookup specific facts about {file}.\n"
-            node = IndexNode(text=file_summary, index_id=file)
-            nodes.append(node)
+            self.agents[file] = OpenAIAgent.from_tools(query_engine_tools, llm=function_llm, verbose=True)
+
+    def create_query_engine(self, agents):
+        """
+        Create a query engine using the specified agents.
+
+        Args:
+            agents (dict): A dictionary of agents to be used in the query engine.
+        """
+        nodes = [
+            IndexNode(text=f"Law Information about {file}", index_id=file)
+            for file in self.docs.keys()
+        ]
 
         vector_index = VectorStoreIndex(nodes)
         vector_retriever = vector_index.as_retriever(similarity_top_k=1)
@@ -69,8 +87,5 @@ class Retriever:
         
         response_synthesizer = get_response_synthesizer(response_mode="compact")
         self.query_engine = RetrieverQueryEngine.from_args(
-            recursive_retriever, response_synthesizer=response_synthesizer, service_context=service_context
+            recursive_retriever, response_synthesizer=response_synthesizer, service_context=self.service_context
         )
-
-
- 
